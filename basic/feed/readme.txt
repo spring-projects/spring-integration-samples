c5fabc21ca558ec27cd9123cadc7190be2a8cf43
Oleg Zhurakousky <ozhurakousky@vmware.com>
2010-11-18 11:33:44 -0500

    INTSAMPLES-11 added FEED sample

---
+++ basic/feed/readme.txt
@@ -0,0 +1,21 @@
+This example demonstrates the following aspects of the FEED (RSS/ATOM) support available with Spring Integration:
+1. FEED Inbound Channel Adapter (receives news feeds)
+
+
+	#### INBOUND CHANNEL ADAPTER ####
+	
+To run INBOUND CHANNEL ADAPTER sample execute FeedInboundChannelAdapterSample test and you'll receive the feed entries for the 
+given URL. Feel free to plug in your own URL
+The output should look similar to this:
+=====
+Thu Nov 18 01:28:54 EST 2010 - Rangel awaits House sanctions
+Thu Nov 18 05:43:11 EST 2010 - Tweet lands woman in China labor camp
+Thu Nov 18 06:13:24 EST 2010 - Bird flu is back in Hong Kong
+Thu Nov 18 07:09:17 EST 2010 - Computer worm called 'game changer'
+Thu Nov 18 08:00:11 EST 2010 - Murkowski declares victory in Alaska
+Thu Nov 18 08:50:04 EST 2010 - Family awaits hanging verdict for mom
+Thu Nov 18 09:14:20 EST 2010 - Suspect bag on Germany-bound plane
+Thu Nov 18 09:47:15 EST 2010 - Zakaria: How serious is this deficit?
+Thu Nov 18 09:51:15 EST 2010 - Two former Madoff employees arrested
+Thu Nov 18 11:15:22 EST 2010 - WikiLeaks founder wanted in rape case
+=====
---
+++ basic/feed/src/test/java/org/springframework/integration/samples/feed/FeedInboundChannelAdapterSample.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.samples.feed;
+
+import org.junit.Test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.Message;
+import org.springframework.integration.core.PollableChannel;
+
+import com.sun.syndication.feed.synd.SyndEntry;
+
+/**
+ * @author Oleg Zhurakousky
+ *
+ */
+public class FeedInboundChannelAdapterSample {
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void runDemo(){
+		ApplicationContext ac = 
+			new ClassPathXmlApplicationContext("META-INF/spring/integration/FeedInboundChannelAdapterSample-context.xml");
+		PollableChannel feedChannel = ac.getBean("feedChannel", PollableChannel.class);
+		for (int i = 0; i < 10; i++) {
+			Message<SyndEntry> message = (Message<SyndEntry>) feedChannel.receive(1000);
+			if (message != null){
+				SyndEntry entry = message.getPayload();
+				System.out.println(entry.getPublishedDate() + " - " + entry.getTitle());
+			}
+			else {
+				break;
+			}
+		}
+	}
+}
