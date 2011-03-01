4392c38ee6f92872d78bdc3d82c3fe13b508bbf8
Oleg Zhurakousky <ozhurakousky@vmware.com>
2011-03-01 09:24:47 -0500

    INT-1821 updated samples POMs to use 2.0.3.RELEASE of SI

--- applications/cafe/src/main/java/org/springframework/integration/samples/cafe/xml/CafeDemoApp.java
+++ applications/cafe/src/main/java/org/springframework/integration/samples/cafe/xml/CafeDemoApp.java
@@ -16,7 +16,6 @@
 
 package org.springframework.integration.samples.cafe.xml;
 
-import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.integration.samples.cafe.Cafe;
