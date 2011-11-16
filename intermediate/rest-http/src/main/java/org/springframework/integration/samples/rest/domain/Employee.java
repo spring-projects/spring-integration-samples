9d5a6f47f25a9aae639eed5e40c45536b8e415a9
Vigil Bose <vigil.bose@gmail.com>
2011-11-16 15:52:37 -0500

    Adding rest-http sample to demontstate the usage of Spring Integration's new HTTP Path facility usage. The JIRA referenced for this sample is https://jira.springsource.org/browse/INTSAMPLES-36

---
+++ intermediate/rest-http/src/main/java/org/springframework/integration/samples/rest/domain/Employee.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.samples.rest.domain;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * Employee.java: Employee Domain class
+ * @author Vigil Bose
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "employeeId",
+    "fname",
+    "lname"
+})
+@XmlRootElement(name = "Customer")
+public class Employee {
+
+	private Integer employeeId;
+	private String fname;
+	private String lname;
+	
+	public Employee() {}
+	
+	public Employee(Integer employeeId, String fname, String lname) {
+		this.employeeId = employeeId;
+		this.fname = fname;
+		this.lname = lname;
+	}
+	/**
+	 * @return the employeeId
+	 */
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+	/**
+	 * @param employeeId the employeeId to set
+	 */
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+	/**
+	 * @return the fname
+	 */
+	public String getFname() {
+		return fname;
+	}
+	/**
+	 * @param fname the fname to set
+	 */
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	/**
+	 * @return the lname
+	 */
+	public String getLname() {
+		return lname;
+	}
+	/**
+	 * @param lname the lname to set
+	 */
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+}
+
+
