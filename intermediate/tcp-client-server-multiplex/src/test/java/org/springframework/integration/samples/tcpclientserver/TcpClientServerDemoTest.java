41419e66d573ef32519a2caac41aee7c31627209
Gary Russell <grussell@vmware.com>
2011-12-21 22:01:39 -0500

    INTSAMPLES-14 Tcp Collaborating Adapters Sample
    Add a sample showing how to use collaborating channel adapters
    for high performance.

---
+++ intermediate/tcp-client-server-multiplex/src/test/java/org/springframework/integration/samples/tcpclientserver/TcpClientServerDemoTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.samples.tcpclientserver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+/**
+ * Demonstrates the use of a gateway as an entry point into the integration flow,
+ * with two pairs of collaborating channel adapters (client and server), which
+ * enables multiplexing multiple messages over the same connection.
+ *
+ * Requires correlation data in the payload.
+ *
+ * @author Gary Russell
+ * @since 2.1
+ *
+ */
+@ContextConfiguration("/META-INF/spring/integration/tcpClientServerDemo-conversion-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TcpClientServerDemoTest {
+
+	@Autowired
+	SimpleGateway gw;
+
+	@Test
+	public void testHappyDay() {
+		String result = gw.send("999Hello world!"); // first 3 bytes is correlationid
+		assertEquals("999Hello world!:echo", result);
+	}
+
+	@Test
+	public void testMultiPlex() throws Exception {
+		TaskExecutor executor = new SimpleAsyncTaskExecutor();
+		final CountDownLatch latch = new CountDownLatch(100);
+		final Set<Integer> results = new HashSet<Integer>();
+		for (int i = 100; i < 200; i++) {
+			results.add(i);
+			final int j = i;
+			executor.execute(new Runnable() {
+				public void run() {
+					String result = gw.send(j + "Hello world!"); // first 3 bytes is correlationid
+					assertEquals(j + "Hello world!:echo", result);
+					results.remove(j);
+					latch.countDown();
+				}});
+		}
+		assertTrue(latch.await(10, TimeUnit.SECONDS));
+		assertEquals(0, results.size());
+	}
+}
