43b85119c82fbcb1805fc17d389ffda1c60ba2ca
Gunnar Hillert <ghillert@vmware.com>
2012-02-10 12:02:55 -0500

    INTSAMPLES-54 - Update README.md
    * Update README.md to reflect correct classes
    * Add documentation for Delayer application
    * Add JavaDoc for delayer application
    * Delete redundant *src/main/resources/log4j.properties*
    * Add logger name to *Logging Channel Adapter* configuration
    * Add Maven Exec Maven Plugin for easy execution from the command line

--- basic/helloworld/src/main/java/org/springframework/integration/samples/helloworld/DelayerApp.java
+++ basic/helloworld/src/main/java/org/springframework/integration/samples/helloworld/DelayerApp.java
@@ -5,11 +5,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DelayerApp {
 
 	/**
-	 * @param args
+	 * Simple application that polls the current system time 2 times every
+	 * 20 seconds (20000 milliseconds).
+	 *
+	 * The resulting message contains the time in milliseconds and the message
+	 * is routed to a Logging Channel Adapter which will print the time to the
+	 * command prompt.
+	 *
+	 * @param args Not used.
 	 */
 	public static void main(String[] args) throws Exception{
 		new ClassPathXmlApplicationContext("META-INF/spring/integration/delay.xml");
-		System.in.read();
 	}
 
 }
