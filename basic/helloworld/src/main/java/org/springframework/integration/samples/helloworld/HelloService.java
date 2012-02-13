b1aecbd91010857e2bd2b7e4af49de49b61f09fb
Gunnar Hillert <ghillert@vmware.com>
2012-02-13 09:46:20 -0500

    INTSAMPLES-54 - Add missing Copyright notice

--- basic/helloworld/src/main/java/org/springframework/integration/samples/helloworld/HelloService.java
+++ basic/helloworld/src/main/java/org/springframework/integration/samples/helloworld/HelloService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ package org.springframework.integration.samples.helloworld;
 
 /**
  * Simple POJO to be referenced from a Service Activator.
- * 
+ *
  * @author Mark Fisher
  */
 public class HelloService {
