b652196d9a2a0722912ac17c79d6c266364817d0
Gunnar Hillert <ghillert@vmware.com>
2012-04-02 10:45:45 -0400

    INTSAMPLES-70 - Sample showing how to process E-mail attachments For reference see: https://jira.springsource.org/browse/INTSAMPLES-70

---
+++ intermediate/mail-attachments/src/main/java/org/springframework/integration/samples/mailattachments/support/EmailParserUtils.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.integration.samples.mailattachments.support;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import javax.mail.BodyPart;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Part;
+import javax.mail.internet.ContentType;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.ParseException;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.springframework.util.Assert;
+
+/**
+ * Utility Class for parsing mail messages.
+ *
+ * @author Gunnar Hillert
+ * @since 2.2
+ *
+ */
+public final class EmailParserUtils {
+
+	private static final Logger LOGGER = Logger.getLogger(EmailParserUtils.class);
+
+	/**
+	 * Parses a mail message. The respective message can either be the root message
+	 * or another message that is attached to another message.
+	 *
+	 * @param directory The directory for storing the message. If null this is the root message.
+	 * @param mailMessage The mail message to be parsed. Must not be null.
+	 * @param emailFragments Must not be null.
+	 */
+	public static final void handleMessage(File directory, javax.mail.Message mailMessage, List<EmailFragment> emailFragments) {
+
+		Assert.notNull(mailMessage, "The mail message to be parsed must not be null.");
+		Assert.notNull(emailFragments, "The collection of emailfragments must not be null.");
+
+		final Object content;
+		final String subject;
+
+		try {
+			content = mailMessage.getContent();
+			subject = mailMessage.getSubject();
+		} catch (IOException e) {
+			throw new IllegalStateException("Error while retrieving the email contents.", e);
+		} catch (MessagingException e) {
+			throw new IllegalStateException("Error while retrieving the email contents.", e);
+		}
+
+		if (directory == null) {
+			directory = new File(subject);
+		} else {
+			directory = new File(directory, subject);
+		}
+
+		if (content instanceof String) {
+
+			emailFragments.add(new EmailFragment(new File(subject), "message.txt", content));
+
+		}  else if (content instanceof Multipart) {
+
+			Multipart multipart = (Multipart) content;
+			handleMultipart(directory, multipart, mailMessage, emailFragments);
+
+		} else {
+			throw new IllegalStateException("This content type is not handled - " + content.getClass().getSimpleName());
+		}
+
+	}
+
+	/**
+	 *
+	 * @param directory Must not be null
+	 * @param multipart Must not be null
+	 * @param mailMessage Must not be null
+	 * @param emailFragments Must not be null
+	 */
+	public static final void handleMultipart(File directory, Multipart multipart, javax.mail.Message mailMessage, List<EmailFragment> emailFragments) {
+
+		Assert.notNull(directory, "The directory must not be null.");
+		Assert.notNull(multipart, "The multipart object to be parsed must not be null.");
+		Assert.notNull(mailMessage, "The mail message to be parsed must not be null.");
+		Assert.notNull(emailFragments, "The collection of emailfragments must not be null.");
+
+		final int count;
+		try {
+			count = multipart.getCount();
+
+			if (LOGGER.isInfoEnabled()) {
+				LOGGER.info(String.format("Number of enclosed BodyPart objects: %s.", count));
+			}
+
+		} catch (MessagingException e) {
+			throw new IllegalStateException("Error while retrieving the number of enclosed BodyPart objects.", e);
+		}
+
+		for (int i = 0; i < count; i++) {
+
+			final BodyPart bp;
+
+			try {
+				bp = multipart.getBodyPart(i);
+			} catch (MessagingException e) {
+				throw new IllegalStateException("Error while retrieving body part.", e);
+			}
+
+			final String contentType;
+			String filename;
+			final String disposition;
+			final String subject;
+
+			try {
+
+				contentType = bp.getContentType();
+				filename    = bp.getFileName();
+				disposition = bp.getDisposition();
+				subject     = mailMessage.getSubject();
+
+				if (filename == null && bp instanceof MimeBodyPart) {
+					filename = ((MimeBodyPart) bp).getContentID();
+				}
+
+			} catch (MessagingException e) {
+				throw new IllegalStateException("Unable to retrieve body part meta data.", e);
+			}
+
+			if (LOGGER.isInfoEnabled()) {
+				LOGGER.info(String.format("BodyPart - Content Type: '%s', filename: '%s', disposition: '%s', subject: '%s'",
+							 new Object[]{contentType, filename, disposition, subject}));
+			}
+
+			if (Part.ATTACHMENT.equalsIgnoreCase(disposition)) {
+				LOGGER.info(String.format("Handdling attachment '%s', type: '%s'", filename, contentType));
+			}
+
+			final Object content;
+
+			try {
+				content = bp.getContent();
+			} catch (IOException e) {
+				throw new IllegalStateException("Error while retrieving the email contents.", e);
+			} catch (MessagingException e) {
+				throw new IllegalStateException("Error while retrieving the email contents.", e);
+			}
+
+			if (content instanceof String) {
+
+				if (Part.ATTACHMENT.equalsIgnoreCase(disposition)) {
+					emailFragments.add(new EmailFragment(directory, i + "-" + filename, content));
+					LOGGER.info(String.format("Handdling attachment '%s', type: '%s'", filename, contentType));
+				} else {
+
+					final String textFilename;
+					ContentType ct;
+					try {
+						ct = new ContentType(contentType);
+					} catch (ParseException e) {
+						throw new IllegalStateException("Error while parsing content type '" + contentType + "'.", e);
+					}
+
+					if ("text/plain".equalsIgnoreCase(ct.getBaseType())) {
+						textFilename = "message.txt";
+					} else if ("text/html".equalsIgnoreCase(ct.getBaseType())) {
+						textFilename = "message.html";
+					} else {
+						textFilename = "message.other";
+					}
+
+					emailFragments.add(new EmailFragment(directory, textFilename, content));
+				}
+
+
+			}  else if (content instanceof InputStream) {
+
+				InputStream inputStream = (InputStream) content;
+				ByteArrayOutputStream bis = new ByteArrayOutputStream();
+
+				try {
+					IOUtils.copy(inputStream, bis);
+				} catch (IOException e) {
+					throw new IllegalStateException("Error while copying input stream to the ByteArrayOutputStream.", e);
+				}
+
+				emailFragments.add(new EmailFragment(directory, filename, bis.toByteArray()));
+
+			} else if (content instanceof javax.mail.Message)  {
+				handleMessage(directory, (javax.mail.Message) content, emailFragments);
+			} else if (content instanceof Multipart) {
+				Multipart mp2 = (Multipart) content;
+				handleMultipart(directory, mp2, mailMessage, emailFragments);
+			} else {
+				throw new IllegalStateException("Content type not handled: " + content.getClass().getSimpleName());
+			}
+		}
+
+	}
+
+
+}
---
+++ intermediate/mail-attachments/src/test/java/org/springframework/integration/samples/mailattachments/MimeMessageParsingTest.java
@@ -0,0 +1,241 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.samples.mailattachments;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.integration.samples.mailattachments.support.EmailFragment;
+import org.springframework.integration.samples.mailattachments.support.EmailParserUtils;
+import org.springframework.mail.MailParseException;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.subethamail.wiser.Wiser;
+import org.subethamail.wiser.WiserMessage;
+
+/**
+ * Test to verify the correct parsing of Email Messages.
+ *
+ * @author Gunnar Hillert
+ * @since 2.2
+ */
+public class MimeMessageParsingTest {
+
+	private static final Logger LOGGER = Logger.getLogger(MimeMessageParsingTest.class);
+
+	private Wiser wiser;
+
+	@Before
+	public void startWiser() {
+		wiser = new Wiser();
+		wiser.setPort(2500);
+		wiser.start();
+		LOGGER.info("Wiser was started.");
+	}
+
+	/**
+	 * This test will create a Mime Message that contains an Attachment, send it
+	 * to an SMTP Server (Using Wiser) and retrieve and process the Mime Message.
+	 *
+	 * This test verifies that the parsing of the retrieved Mime Message is
+	 * successful and that the correct number of {@link EmailFragment}s is created.
+	 */
+	@Test
+	public void testProcessingOfEmailAttachments() {
+
+		final JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setPort(2500);
+
+		final MimeMessage message = mailSender.createMimeMessage();
+		final String pictureName = "picture.png";
+
+		final ByteArrayResource byteArrayResource = getFileData(pictureName);
+
+		try {
+
+			final MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+			helper.setFrom("testfrom@springintegration.org");
+			helper.setTo("testto@springintegration.org");
+			helper.setSubject("Parsing of Attachments");
+			helper.setText("Spring Integration Rocks!");
+
+			helper.addAttachment(pictureName, byteArrayResource, "image/png");
+
+		} catch (MessagingException e) {
+			throw new MailParseException(e);
+		}
+
+		mailSender.send(message);
+
+		final List<WiserMessage> wiserMessages = wiser.getMessages();
+
+		Assert.assertTrue(wiserMessages.size() == 1);
+
+		boolean foundTextMessage = false;
+		boolean foundPicture = false;
+
+		for (WiserMessage wiserMessage : wiserMessages) {
+
+				List<EmailFragment> emailFragments = new ArrayList<EmailFragment>();
+
+				try {
+
+					final MimeMessage mailMessage = wiserMessage.getMimeMessage();
+					EmailParserUtils.handleMessage(null, mailMessage, emailFragments);
+
+				} catch (MessagingException e) {
+					throw new IllegalStateException("Error while retrieving Mime Message.");
+				}
+
+				Assert.assertTrue(emailFragments.size() == 2);
+
+				for (EmailFragment emailFragment : emailFragments) {
+					if ("picture.png".equals(emailFragment.getFilename())) {
+						foundPicture = true;
+					}
+
+					if ("message.txt".equals(emailFragment.getFilename())) {
+						foundTextMessage = true;
+					}
+				}
+
+				Assert.assertTrue(foundPicture);
+				Assert.assertTrue(foundTextMessage);
+
+		}
+	}
+
+	/**
+	 * This test will create a Mime Message that in return contains another
+	 * mime message. The nested mime message contains an attachment.
+	 *
+	 * The root message consist of both HTML and Text message.
+	 *
+	 */
+	@Test
+	public void testProcessingOfNestedEmailAttachments() {
+
+		final JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setPort(2500);
+
+		final MimeMessage rootMessage = mailSender.createMimeMessage();
+
+		try {
+
+			final MimeMessageHelper messageHelper = new MimeMessageHelper(rootMessage, true);
+
+			messageHelper.setFrom("testfrom@springintegration.org");
+			messageHelper.setTo("testto@springintegration.org");
+			messageHelper.setSubject("Parsing of Attachments");
+			messageHelper.setText("Spring Integration Rocks!", "Spring Integration <b>Rocks</b>!");
+
+			final MimeMessage message = mailSender.createMimeMessage();
+			final String pictureName = "picture.png";
+
+			final ByteArrayResource byteArrayResource = getFileData(pictureName);
+
+			messageHelper.addInline("picture12345", byteArrayResource, "image/png");
+
+		} catch (MessagingException e) {
+			throw new MailParseException(e);
+		}
+
+		mailSender.send(rootMessage);
+
+		final List<WiserMessage> wiserMessages = wiser.getMessages();
+
+		Assert.assertTrue(wiserMessages.size() == 1);
+
+		boolean foundTextMessage = false;
+		boolean foundPicture = false;
+		boolean foundHtmlMessage = false;
+
+		for (WiserMessage wiserMessage : wiserMessages) {
+
+			List<EmailFragment> emailFragments = new ArrayList<EmailFragment>();
+
+			try {
+
+				final MimeMessage mailMessage = wiserMessage.getMimeMessage();
+				EmailParserUtils.handleMessage(null, mailMessage, emailFragments);
+
+			} catch (MessagingException e) {
+				throw new IllegalStateException("Error while retrieving Mime Message.");
+			}
+
+			Assert.assertTrue(emailFragments.size() == 3);
+
+			for (EmailFragment emailFragment : emailFragments) {
+				if ("<picture12345>".equals(emailFragment.getFilename())) {
+					foundPicture = true;
+				}
+
+				if ("message.txt".equals(emailFragment.getFilename())) {
+					foundTextMessage = true;
+				}
+
+				if ("message.html".equals(emailFragment.getFilename())) {
+					foundHtmlMessage = true;
+				}
+			}
+
+			Assert.assertTrue(foundPicture);
+			Assert.assertTrue(foundTextMessage);
+			Assert.assertTrue(foundHtmlMessage);
+
+		}
+	}
+
+	private ByteArrayResource getFileData(String filename) {
+
+		final InputStream attachmentInputStream = MimeMessageParsingTest.class.getResourceAsStream(filename);
+
+		Assert.assertNotNull("Resource not found: " + filename, attachmentInputStream);
+
+		ByteArrayResource byteArrayResource = null;
+
+		try {
+			byteArrayResource = new ByteArrayResource(IOUtils.toByteArray(attachmentInputStream));
+			attachmentInputStream.close();
+		} catch (IOException e1) {
+			Assert.fail();
+		}
+
+		return byteArrayResource;
+
+	}
+
+	@After
+	public void stopWiser() {
+		wiser.stop();
+		LOGGER.info("Wiser stopped.");
+	}
+
+}
