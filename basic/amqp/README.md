b21c633fb8768cb24082e4783848f05a048647f5
Gary Russell <grussell@gopivotal.com>
2014-06-23 09:42:50 -0400

    INTSAMPLES-132 AMQP Publisher Confirms/Returns
    JIRA: https://jira.spring.io/browse/INTSAMPLES-132

--- basic/amqp/README.md
+++ basic/amqp/README.md
@@ -15,9 +15,39 @@ If you imported the example into your IDE, you can just run class **org.springfr
 
 * Right-click on Main class --> Run As --> Java Application
 
+* ./gradlew :amqp:run
+
 Alternatively, you can start the sample from the command line ([Gradle](http://www.gradle.org) required):
 
-* gradlew :amqp:run
+The __Main2__ class is similar, but demonstrates publisher confirms and returns.
+
+* Right-click on Main2 class --> Run As --> Java Application
+
+* ./gradlew :amqp:runMain2
+
+When you enter a message in the console you will see the message received, together with a send confirmation:
+
+````
+foo
+Received: foo
+foo sent ok
+````
+
+When you enter 'fail', the message is sent with a bad routing key; you will see the message is sent ok, but returned because it is not routable:
+
+````
+fail
+fail returned:NO_ROUTE
+fail sent ok
+````
+
+When you enter 'nack', the message is sent to a non-existent exchange; the broker reacts to this by closing the channel with an error and Spring AMQP generates a Nack:
+
+````
+nack
+nack send failed (nack)
+11:54:00.818 ERROR [pool-1-thread-1][org.springframework.amqp.rabbit.connection.CachingConnectionFactory] Channel shutdown: channel error; protocol method: #method<channel.close>(reply-code=404, reply-text=NOT_FOUND - no exchange 'badExchange' in vhost '/', class-id=60, method-id=40)
+````
 
 # Used Spring Integration components
 
--- basic/amqp/src/main/java/org/springframework/integration/samples/amqp/Main2.java
+++ basic/amqp/src/main/java/org/springframework/integration/samples/amqp/Main2.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,15 +23,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * Starts the Spring Context and will initialize the Spring Integration message flow.
  *
- * @author Gunnar Hillert
- * @version 1.0
+ * @author Gary Russell.
+ * @version 4.0
  *
  */
-public final class Main {
+public final class Main2 {
 
-	private static final Logger LOGGER = Logger.getLogger(Main.class);
+	private static final Logger LOGGER = Logger.getLogger(Main2.class);
 
-	private Main() { }
+	private Main2() { }
 
 	/**
 	 * Load the Spring Integration Application Context
@@ -50,18 +50,23 @@ public final class Main {
 				  + "\n=========================================================" );
 
 		final AbstractApplicationContext context =
-				new ClassPathXmlApplicationContext("classpath:META-INF/spring/integration/spring-integration-context.xml");
+				new ClassPathXmlApplicationContext("classpath:META-INF/spring/integration/spring-integration-confirms-context.xml");
 
 		context.registerShutdownHook();
 
 		LOGGER.info("\n========================================================="
 				  + "\n                                                          "
-				  + "\n    This is the AMQP Sample -                             "
+				  + "\n    This is the AMQP Sample with confirms/returns -       "
 				  + "\n                                                          "
 				  + "\n    Please enter some text and press return. The entered  "
 				  + "\n    Message will be sent to the configured RabbitMQ Queue,"
 				  + "\n    then again immediately retrieved from the Message     "
 				  + "\n    Broker and ultimately printed to the command line.    "
+				  + "\n    Send 'fail' to demonstrate a return because the       "
+				  + "\n    message couldn't be routed to a queue.                "
+				  + "\n    Send 'nack' to demonstrate a NACK because the         "
+				  + "\n    exchange doesn't exist, causing the channel to be     "
+				  + "\n    closed in error by the broker.                        "
 				  + "\n                                                          "
 				  + "\n=========================================================" );
 
