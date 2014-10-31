5d08c6f67ee387f51c7bf358b7f95228f7a44bb8
Artem Bilan <abilan@pivotal.io>
2014-10-31 21:56:23 +0200

    Upgrade to SI Java DSL 1.0.RC1 and Gradle 1.12
    Regenerate POMs

--- dsl/cafe-dsl/src/main/java/org/springframework/integration/samples/dsl/cafe/Application.java
+++ dsl/cafe-dsl/src/main/java/org/springframework/integration/samples/dsl/cafe/Application.java
@@ -33,7 +33,7 @@ import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.channel.MessageChannels;
-import org.springframework.integration.dsl.support.Pollers;
+import org.springframework.integration.dsl.core.Pollers;
 import org.springframework.integration.samples.cafe.Delivery;
 import org.springframework.integration.samples.cafe.Drink;
 import org.springframework.integration.samples.cafe.DrinkType;
