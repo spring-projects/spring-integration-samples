17160668bdd6501f5ddd5c5cbec88e3c52941eb7
Gary Russell <grussell@pivotal.io>
2015-08-17 06:54:07 -0400

    Fix SFTP, @Ignore Travel Sample Test
    See INTSAMPLES-145

--- intermediate/travel/src/test/java/org/springframework/integration/samples/travel/TravelGatewayTest.java
+++ intermediate/travel/src/test/java/org/springframework/integration/samples/travel/TravelGatewayTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,10 @@ package org.springframework.integration.samples.travel;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -41,7 +43,7 @@ public class TravelGatewayTest {
 	@Autowired
 	private TravelGateway travelGateway;
 
-	@Test
+	@Test @Ignore
 	public void testGetWeatherByCity() {
 		final String weatherInformation = travelGateway.getWeatherByCity(City.ATLANTA);
 		LOGGER.info("Weather information for Atlanta:\n\n" + weatherInformation + "\n\n");
