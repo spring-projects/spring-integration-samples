f8634557bb4c34822ff3c5edf82a37d163596083
Artem Bilan <abilan@pivotal.io>
2015-09-09 17:03:40 -0400

    Upgrade to SI-4.2.0.RELEASE
    And SI-Java-DSL-1.1.0.M2

--- dsl/cafe-dsl/src/main/java/org/springframework/integration/samples/dsl/cafe/lambda/Application.java
+++ dsl/cafe-dsl/src/main/java/org/springframework/integration/samples/dsl/cafe/lambda/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2014-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -118,7 +118,7 @@ public class Application {
 										.stream()
 										.map(message -> (Drink) message.getPayload())
 										.collect(Collectors.toList())))
-						.correlationStrategy(m -> ((Drink) m.getPayload()).getOrderNumber()), null)
+						.correlationStrategy(m -> ((Drink) m.getPayload()).getOrderNumber()))
 				.handle(CharacterStreamWritingMessageHandler.stdout());
 	}
 
