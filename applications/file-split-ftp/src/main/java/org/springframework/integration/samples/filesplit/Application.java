434e017ae1430d80103a7e9929bc982a93fe343b
Gary Russell <grussell@pivotal.io>
2016-09-20 14:32:58 -0400

    Add file-split-ftp Sample
    JIRA: https://jira.spring.io/browse/INTSAMPLES-149

---
+++ applications/file-split-ftp/src/main/java/org/springframework/integration/samples/filesplit/Application.java
@@ -0,0 +1,238 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.samples.filesplit;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.apache.commons.net.ftp.FTPFile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.dsl.file.Files;
+import org.springframework.integration.dsl.mail.Mail;
+import org.springframework.integration.dsl.support.StringStringMapBuilder;
+import org.springframework.integration.file.FileHeaders;
+import org.springframework.integration.file.remote.session.SessionFactory;
+import org.springframework.integration.file.splitter.FileSplitter;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.integration.ftp.session.DefaultFtpSessionFactory;
+import org.springframework.integration.http.config.EnableIntegrationGraphController;
+import org.springframework.integration.router.PayloadTypeRouter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.MessagingException;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+@EnableConfigurationProperties(MailProperties.class)
+@EnableIntegrationGraphController
+public class Application {
+
+	private static final String EMAIL_SUCCESS_SUFFIX = "emailSuccessSuffix";
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	private MailProperties mailProperties;
+
+	@Bean
+	public IntegrationFlow fromFile() {
+		return IntegrationFlows.from(Files.inboundAdapter(new File("/tmp/in"))
+					.patternFilter("*.txt"), e -> e.poller(Pollers.fixedDelay(5000)))
+				.enrichHeaders(h -> h.header(MessageHeaders.ERROR_CHANNEL, "tfrErrors.input"))
+				.handle(Files.splitter(true, true))
+				.route(ptr())
+				.get();
+	}
+
+	private PayloadTypeRouter ptr() {
+		PayloadTypeRouter payloadTypeRouter = new PayloadTypeRouter();
+		payloadTypeRouter.setChannelMappings(new StringStringMapBuilder()
+				.put(FileSplitter.FileMarker.class.getName(), "markers.input")
+				.put(String.class.getName(), "lines.input")
+				.get());
+		return payloadTypeRouter;
+	}
+
+	/**
+	 * Process lines; append (no flush) to the appropriate file.
+	 * @return the flow.
+	 */
+	@Bean
+	public IntegrationFlow lines() {
+		return f -> f.enrichHeaders(h -> h.headerExpression(
+						FileHeaders.FILENAME, "payload.substring(1, 4) + '.txt'", true))
+				.handle(Files.outboundAdapter("'/tmp/out'")
+						.appendNewLine(true)
+						.fileExistsMode(FileExistsMode.APPEND_NO_FLUSH), // files remain open for efficiency
+							e -> e.id("fileOut"));
+	}
+
+	/**
+	 * Process file markers; ignore START, when END, flush the files, ftp them and
+	 * send an email.
+	 * @return the flow.
+	 */
+	@Bean
+	public IntegrationFlow markers() {
+		return f -> f.<FileSplitter.FileMarker>filter(m -> m.getMark().equals(FileSplitter.FileMarker.Mark.END),
+						e -> e.id("markerFilter").discardChannel("nullChannel")) // INT-4109
+				.publishSubscribeChannel(s -> s
+
+						// first trigger file flushes
+						.subscribe(sf -> sf.transform("'/tmp/out/.*\\.txt'")
+										.handle("fileOut.handler", "trigger", e -> e.id("flusher")))
+
+						// send the first file
+						.subscribe(sf -> sf.<FileSplitter.FileMarker, File>transform(p -> new File("/tmp/out/002.txt"))
+								.enrichHeaders(h -> h.header(FileHeaders.FILENAME, "002.txt", true))
+								.handleWithAdapter(a -> a.ftp(ftp1()).remoteDirectory("foo"), e -> e.id("ftp002")))
+
+						// send the second file
+						.subscribe(sf -> sf.<FileSplitter.FileMarker, File>transform(p -> new File("/tmp/out/006.txt"))
+								.enrichHeaders(h -> h.header(FileHeaders.FILENAME, "006.txt", true))
+								.handleWithAdapter(a -> a.ftp(ftp2()).remoteDirectory("foo"), e -> e.id("ftp006")))
+
+						// send the third file
+						.subscribe(sf -> sf.<FileSplitter.FileMarker, File>transform(p -> new File("/tmp/out/009.txt"))
+								.enrichHeaders(h -> h.header(FileHeaders.FILENAME, "009.txt", true))
+								.handleWithAdapter(a -> a.ftp(ftp3()).remoteDirectory("foo"), e -> e.id("ftp009")))
+
+						// send an email
+						.subscribe(sf -> sf.<FileSplitter.FileMarker, String>transform(p -> p.getFilePath())
+								.enrichHeaders(Mail.headers()
+										.subject("File successfully split and transferred")
+										.from("foo@bar")
+										.toFunction(m -> new String[] {"bar@baz"}))
+								.enrichHeaders(h -> h.header(EMAIL_SUCCESS_SUFFIX, ".success"))
+								.channel("toMail.input")));
+	}
+
+	@Bean
+	public SessionFactory<FTPFile> ftp1() {
+		DefaultFtpSessionFactory ftp = new DefaultFtpSessionFactory();
+		ftp.setHost("host3");
+		ftp.setUsername("user");
+		ftp.setPassword("ftp");
+		return ftp;
+	}
+
+	@Bean
+	public SessionFactory<FTPFile> ftp2() {
+		DefaultFtpSessionFactory ftp = new DefaultFtpSessionFactory();
+		ftp.setHost("host3");
+		ftp.setUsername("user");
+		ftp.setPassword("ftp");
+		return ftp;
+	}
+
+	@Bean
+	public SessionFactory<FTPFile> ftp3() {
+		DefaultFtpSessionFactory ftp = new DefaultFtpSessionFactory();
+		ftp.setHost("host3");
+		ftp.setUsername("user");
+		ftp.setPassword("ftp");
+		return ftp;
+	}
+
+	/**
+	 * Error flow - email failure
+	 * @return the flow.
+	 */
+	@Bean
+	public IntegrationFlow tfrErrors() {
+		return f -> f.enrichHeaders(Mail.headers()
+					.subject("File split and transfer failed")
+					.from("foo@bar")
+					.toFunction(m -> new String[] {"bar@baz"}))
+				.enrichHeaders(h -> h.header(EMAIL_SUCCESS_SUFFIX, ".failed")
+						.headerExpression(FileHeaders.ORIGINAL_FILE, "payload.failedMessage.headers['"
+								+ FileHeaders.ORIGINAL_FILE + "']"))
+				.<MessagingException, String>transform(p -> {
+					StringBuilder builder = new StringBuilder();
+					builder.append(p.getFailedMessage().getPayload().toString()).append("\n")
+						.append(getStackTraceAsString(p));
+					return builder.toString();
+				})
+				.channel("toMail.input");
+	}
+
+	@Bean
+	public IntegrationFlow toMail() {
+		return f ->  f.handleWithAdapter(a -> a.mail(this.mailProperties.getHost())
+						.javaMailProperties(b -> b.put("mail.debug", "true"))
+						.port(this.mailProperties.getPort())
+						.credentials(this.mailProperties.getUsername(), this.mailProperties.getPassword()),
+						e -> e.id("mailOut").advice(afterMailAdvice()));
+	}
+
+	/**
+	 * Rename the input file after success/failure.
+	 * @return the flow.
+	 */
+	@Bean
+	public MethodInterceptor afterMailAdvice() {
+		return invocation -> {
+			Message<?> message = (Message<?>) invocation.getArguments()[0];
+			MessageHeaders headers = message.getHeaders();
+			File originalFile = headers.get(FileHeaders.ORIGINAL_FILE, File.class);
+			try {
+				invocation.proceed();
+				originalFile.renameTo(
+						new File(originalFile.getAbsolutePath() + headers.get(EMAIL_SUCCESS_SUFFIX)));
+			}
+			catch (Exception e) {
+				originalFile.renameTo(
+						new File(originalFile.getAbsolutePath() + headers.get(EMAIL_SUCCESS_SUFFIX) + ".email.failed"));
+			}
+			return null;
+		};
+	}
+
+	// Integration Graph CORS
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/integration").allowedOrigins("http://localhost:8082");
+			}
+		};
+	}
+
+	private String getStackTraceAsString(Throwable cause) {
+		StringWriter stringWriter = new StringWriter();
+		PrintWriter printWriter = new PrintWriter(stringWriter, true);
+		cause.printStackTrace(printWriter);
+		return stringWriter.getBuffer().toString();
+	}
+
+}
---
+++ applications/file-split-ftp/src/test/java/org/springframework/integration/samples/filesplit/ApplicationTests.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.samples.filesplit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.net.ftp.FTPFile;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.integration.file.remote.session.Session;
+import org.springframework.integration.file.remote.session.SessionFactory;
+import org.springframework.integration.samples.filesplit.Application;
+import org.springframework.integration.test.mail.TestMailServer;
+import org.springframework.integration.test.mail.TestMailServer.SmtpServer;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	private static final SmtpServer smtpServer = TestMailServer.smtp(0);
+
+	@Autowired
+	private Session<FTPFile> session;
+
+	@BeforeClass
+	public static void setup() {
+		// Configure the boot property to send email to the test email server.
+		System.setProperty("spring.mail.port", Integer.toString(smtpServer.getPort()));
+		new File("/tmp/in/foo.txt").delete();
+		new File("/tmp/in/foo.txt.success").delete();
+		new File("/tmp/in/foo.txt.failed").delete();
+	}
+
+	@Before
+	public void beforeTest() {
+		smtpServer.getMessages().clear();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testSuccess() throws Exception {
+		reset(this.session);
+		String message = runTest(false);
+		assertThat(message).contains("File successfully split and transferred");
+		assertThat(message).contains("/tmp/in/foo.txt");
+	}
+
+	@Test
+	public void testFailure() throws Exception {
+		willThrow(new RuntimeException("fail test exception"))
+			.given(this.session).write(any(InputStream.class), eq("foo/002.txt.writing"));
+		String message = runTest(true);
+		assertThat(message).contains("File split and transfer failed");
+		assertThat(message).contains("fail test exception");
+		assertThat(message).contains("/tmp/out/002.txt");
+	}
+
+	/*
+	 * Create a test file containing one row per account.
+	 * Verify the three files appear, with the correct contents.
+	 * Verify the input file was renamed based on success/failure.
+	 * Verify the email was sent.
+	 */
+	private String runTest(boolean fail) throws FileNotFoundException, IOException, InterruptedException, Exception {
+		File in = new File("/tmp/in/", "foo");
+		FileOutputStream fos = new FileOutputStream(in);
+		fos.write("*002,foo,bar\n*006,baz,qux\n*009,fiz,buz\n".getBytes());
+		fos.close();
+		in.renameTo(new File("/tmp/in/", "foo.txt"));
+		File out = new File("/tmp/out/002.txt");
+		int n = 0;
+		while(n++ < 100 && (!out.exists() || out.length() < 12)) {
+			Thread.sleep(100);
+		}
+		assertThat(out.exists()).isTrue();
+		BufferedReader br = new BufferedReader(new FileReader(out));
+		assertThat(br.readLine()).isEqualTo("*002,foo,bar");
+		br.close();
+		out.delete();
+		out = new File("/tmp/out/006.txt");
+		assertThat(out.exists()).isTrue();
+		br = new BufferedReader(new FileReader(out));
+		assertThat(br.readLine()).isEqualTo("*006,baz,qux");
+		br.close();
+		out.delete();
+		out = new File("/tmp/out/009.txt");
+		assertThat(out.exists()).isTrue();
+		br = new BufferedReader(new FileReader(out));
+		assertThat(br.readLine()).isEqualTo("*009,fiz,buz");
+		br.close();
+		out.delete();
+		if (!fail) {
+			in = new File("/tmp/in/", "foo.txt.success");
+		}
+		else {
+			in = new File("/tmp/in/", "foo.txt.failed");
+		}
+		n = 0;
+		while(n++ < 100 && !in.exists()) {
+			Thread.sleep(100);
+		}
+		assertThat(in.exists()).isTrue();
+		in.delete();
+		// verify FTP
+		verify(this.session).write(any(InputStream.class), eq("foo/002.txt.writing"));
+		if (!fail) {
+			verify(this.session).write(any(InputStream.class), eq("foo/006.txt.writing"));
+			verify(this.session).write(any(InputStream.class), eq("foo/009.txt.writing"));
+			verify(this.session).rename("foo/002.txt.writing", "foo/002.txt");
+			verify(this.session).rename("foo/006.txt.writing", "foo/006.txt");
+			verify(this.session).rename("foo/009.txt.writing", "foo/009.txt");
+		}
+
+		String message = verifyMail();
+		assertThat(message).contains("From: foo@bar");
+		assertThat(message).contains("To: bar@baz");
+		return message;
+	}
+
+	public String verifyMail() throws Exception {
+		List<String> messages = smtpServer.getMessages();
+		int n = 0;
+		while (n++ < 100 && messages.size() < 1) {
+			Thread.sleep(100);
+		}
+		assertThat(messages).hasSize(1);
+		return messages.get(0);
+	}
+
+	/**
+	 * Overrides the ftp session factories with mocks.
+	 *
+	 */
+	@Configuration
+	@Import(Application.class)
+	public static class Config {
+
+		@Bean
+		public SessionFactory<FTPFile> ftp1() throws IOException {
+			return mockSf();
+		}
+
+		@Bean
+		public SessionFactory<FTPFile> ftp2() throws IOException {
+			return mockSf();
+		}
+
+		@Bean
+		public SessionFactory<FTPFile> ftp3() throws IOException {
+			return mockSf();
+		}
+
+		private SessionFactory<FTPFile> mockSf() throws IOException {
+			@SuppressWarnings("unchecked")
+			SessionFactory<FTPFile> mocksf = mock(SessionFactory.class);
+			given(mocksf.getSession()).willReturn(mockSession());
+			return mocksf;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Bean
+		public Session<FTPFile> mockSession() throws IOException {
+			return mock(Session.class);
+		}
+
+	}
+
+}
