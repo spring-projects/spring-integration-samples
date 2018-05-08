8e12cd74327a59a5574d29ca322235c556de9b1c
Gary Russell <grussell@pivotal.io>
2018-05-08 16:38:18 -0400

    Upgrade to SI 4.3.16, SF 4.3.17

--- basic/jpa/src/main/java/org/springframework/integration/samples/jpa/OpenJpaAutoConfiguration.java
+++ basic/jpa/src/main/java/org/springframework/integration/samples/jpa/OpenJpaAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package org.springframework.integration.samples.jpa;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
@@ -29,6 +30,7 @@ import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
@@ -45,6 +47,7 @@ import org.springframework.util.ClassUtils;
 
 /**
  * @author Artem Bilan
+ * @author Gary Russell
  * @since 4.2
  */
 @Configuration
@@ -56,8 +59,9 @@ import org.springframework.util.ClassUtils;
 public class OpenJpaAutoConfiguration extends JpaBaseConfiguration {
 
 	public OpenJpaAutoConfiguration(DataSource dataSource, JpaProperties properties,
-			ObjectProvider<JtaTransactionManager> jtaTransactionManagerProvider) {
-		super(dataSource, properties, jtaTransactionManagerProvider);
+			ObjectProvider<JtaTransactionManager> jtaTransactionManagerProvider,
+			ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
+		super(dataSource, properties, jtaTransactionManagerProvider, transactionManagerCustomizers);
 	}
 
 	@Override
