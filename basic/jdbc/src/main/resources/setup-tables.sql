c868afb039e8529ea609bc248ff3052b8f28d10f
Gary Russell <grussell@pivotal.io>
2018-11-24 11:39:03 -0500

    JDBC Sample Polishing
    - no need for DUMMY table
    - max-rows-per-poll is deprecated
    - set reply-timeout to 0 (for no person result)

--- basic/jdbc/src/main/resources/setup-tables.sql
+++ basic/jdbc/src/main/resources/setup-tables.sql
@@ -1,5 +1,4 @@
 create table IF NOT EXISTS USERS(USERNAME varchar(100),PASSWORD varchar(100), EMAIL varchar(100));
-create table IF NOT EXISTS DUMMY(DUMMY_VALUE varchar(10));
 create table IF NOT EXISTS Person(id integer identity primary key , name varchar(100), gender varchar(1), dateOfBirth date);
 INSERT INTO USERS(USERNAME, PASSWORD, EMAIL) VALUES ('a', 'secret', 'spring-integration@awesome.com');
 INSERT INTO USERS(USERNAME, PASSWORD, EMAIL) VALUES ('b', 's3cr3t', 'spring@rocks.com');
