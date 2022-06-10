0b83398a9cdca60c6c5f00f39afc49aa1329b3f4
Artem Bilan <abilan@vmware.com>
2022-06-10 15:42:03 -0400

    Some JmsMockTests tweaks

--- advanced/advanced-testing-examples/src/test/java/org/springframework/integration/samples/advance/testing/jms/JmsMockTests.java
+++ advanced/advanced-testing-examples/src/test/java/org/springframework/integration/samples/advance/testing/jms/JmsMockTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -162,7 +162,7 @@ public class JmsMockTests {
 	 */
 	protected boolean verifyJmsMessageReceivedOnOutputChannel(Object obj, SubscribableChannel expectedOutputChannel,
 			CountDownHandler handler) throws InterruptedException {
-		return verifyJmsMessageOnOutputChannel(obj, expectedOutputChannel, handler, 7000);
+		return verifyJmsMessageOnOutputChannel(obj, expectedOutputChannel, handler, 10000);
 	}
 
 
