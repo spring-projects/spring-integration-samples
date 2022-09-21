021186e79ca78f98cbb0a990c07c9e4a628002ee
Artem Bilan <abilan@vmware.com>
2022-09-20 21:01:40 -0400

    Upgrade to Spring Integration 6.0.0-M5

--- basic/jms/src/test/java/org/springframework/integration/samples/jms/ActiveMQMultiContextTests.java
+++ basic/jms/src/test/java/org/springframework/integration/samples/jms/ActiveMQMultiContextTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,7 +63,7 @@ public abstract class ActiveMQMultiContextTests {
 						.setJMXManagementEnabled(false)
 						.setJournalDatasync(false)
 						.addAcceptorConfiguration(new TransportConfiguration(InVMAcceptorFactory.class.getName()))
-						.addAddressesSetting("#",
+						.addAddressSetting("#",
 								new AddressSettings()
 										.setDeadLetterAddress(SimpleString.toSimpleString("dla"))
 										.setExpiryAddress(SimpleString.toSimpleString("expiry")));
