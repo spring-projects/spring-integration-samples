1350e9e6d199de7c8a46dc89d50879038ef2113e
Glenn Renfro <glenn.renfro@broadcom.com>
2026-03-20 13:57:56 -0400

    Add gRPC client sample with Spring Integration
    Implement a new basic sample demonstrating Spring Integration's gRPC
    client capabilities. This sample shows how to integrate gRPC with
    Spring Boot and Spring Integration for both single-response and
    streaming response patterns.
    
    This example is based on the proto file that exists in the Spring
    gRPC project, to provide user familiarity
    
    * Implement code review changes
    
    * Remove version from the integration-grpc dependency
    
    * Code review response
    
    - Config uses GrpcHeaders.SERVICE_METHOD for method def
    - Removed amqp dependency update
    - Removed changes to barrier pom.xml
    - spring grpc now uses bom
    
    * Update based on code review
    
    - One flow now sets method at flow definition time
    - Tests updated to not use port 9090
    - Tests disable netty server as it is not needed (and opens port unnecessarily)
    - Readme updated
    - Add pom.xml
    - Rebased
    
    * Remove the netty bean from test
    
    exclude netty dependencies from test

---
+++ basic/grpc-client/src/test/java/org/springframework/integration/samples/grpc/client/GrpcClientTests.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2026-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.samples.grpc.client;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+/**
+ * Integration test for gRPC client functionality.
+ *
+ * @author Glenn Renfro
+ */
+@SpringBootTest(properties = {
+		"spring.grpc.test.inprocess.enabled=true",
+		"spring.grpc.test.inprocess.exclusive=true"
+})
+@DirtiesContext
+@ExtendWith(OutputCaptureExtension.class)
+public class GrpcClientTests {
+
+	/**
+	 * Verify the gRPC service returns the expected greeting message.
+	 * @param output the captured console output
+	 */
+	@Test
+	void verifyMessage(CapturedOutput output) {
+		await()
+				.atMost(Duration.ofSeconds(5))
+				.untilAsserted(() -> {
+					assertThat(output.getAll())
+							.contains("Single response reply: Hello Jack")
+							.contains("Stream received reply: Hello Jane")
+							.contains("Stream received reply: Hello Again");
+				});
+	}
+
+	@TestConfiguration(proxyBeanMethods = false)
+	static class TestGrpcServerConfig {
+
+		/**
+		 * Create a test gRPC service implementation for the Simple service.
+		 * @return the gRPC service implementation
+		 */
+		@Bean
+		SimpleGrpc.SimpleImplBase simpleService() {
+			return new SimpleGrpc.SimpleImplBase() {
+
+				@Override
+				public void sayHello(HelloRequest request, io.grpc.stub.StreamObserver<HelloReply> responseObserver) {
+					HelloReply reply = HelloReply.newBuilder()
+							.setMessage("Hello " + request.getName())
+							.build();
+					responseObserver.onNext(reply);
+					responseObserver.onCompleted();
+				}
+
+				@Override
+				public void streamHello(HelloRequest request,
+						io.grpc.stub.StreamObserver<HelloReply> responseObserver) {
+
+					HelloReply reply = HelloReply.newBuilder()
+							.setMessage("Hello " + request.getName())
+							.build();
+					responseObserver.onNext(reply);
+					reply = HelloReply.newBuilder()
+							.setMessage("Hello Again")
+							.build();
+					responseObserver.onNext(reply);
+					responseObserver.onCompleted();
+				}
+			};
+		}
+	}
+
+}
