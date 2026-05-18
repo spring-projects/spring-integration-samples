482fe015a5edd8e1de1e40f4e60fc8b06bda804f
Artem Bilan <artem.bilan@broadcom.com>
2026-05-18 17:56:26 -0400

    Ignore empty `DynamicTcpClientApplicationTests`

--- advanced/dynamic-tcp-client/src/test/java/org/springframework/integration/samples/dynamictcp/DynamicTcpClientApplicationTests.java
+++ advanced/dynamic-tcp-client/src/test/java/org/springframework/integration/samples/dynamictcp/DynamicTcpClientApplicationTests.java
@@ -1,10 +1,12 @@
 package org.springframework.integration.samples.dynamictcp;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DynamicTcpClientApplicationTests {
